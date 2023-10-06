39b42c577373a876f2ce714ab6f107c341070f9d
Floupper <116433624+Floupper@users.noreply.github.com>
2023-10-06 17:59:21 +0200

    Delete w31 directory

--- w31/CorrectionTP3/modules/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-  $db = new PDO("sqlite:../database.db");
-  $db->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-  die("Error: ".$e->getMessage()."!");
-}
--- w31/TP2/bdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$users = [
-    'mathieu' => 'matmat',
-    'leo' => 'leonnoute',
-    'greg' => 'gregouille'
-];
